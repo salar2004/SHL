2566e9e61a0ddad3a00908d7655d058c45046686
Hajar <hajaralshawy@gmail.com>
2025-02-20 21:25:38 +0300

    updateAuth

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
